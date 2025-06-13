5504ea6488b668f6e24ca2a71d35b13f3e465a13
Ivo <91078@sistemas.frc.utn.edu.ar>
2025-06-13 11:04:22 -0300

    Llegue hasta el video 5 sin incluir

---
+++ addons/utn_addons/utn_hospital/__init__.py
@@ -0,0 +1,2 @@
+# Hospital Management UTN
+# Archivo de inicialización del módulo
---
+++ addons/utn_addons/utn_hospital/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Hospital Management UTN',
+    'version': '17.0.1.0.0',
+    'sequence': -1200,
+    'category': 'Healthcare',
+    'summary': 'Modulo de ejemplo para la gestión de un hospital',
+    'description': 'Descripción larga del módulo hospitalario',
+    'author': 'Ivo',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/hospital_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+}
